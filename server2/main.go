8456c827cbce6f2f47659b91b8ea2b69b8367639
Richard Bono <Richard.Bono.CTR@aiworldwide.com>
2022-08-24 15:18:26 +0200

    Update workflow

--- server2/main.go
+++ server2/main.go
@@ -6,5 +6,5 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("<h1>Full Cycle Esquenta server2</h1>"))
 	})
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8082", nil)
 }
--- server4/main.go
+++ server4/main.go
@@ -6,5 +6,5 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("<h1>Full Cycle Esquenta server 4</h1>"))
 	})
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8084", nil)
 }

8bd424d8a3305d9ca8f8a74e3ae6416b2800822c
Richard Bono <Richard.Bono.CTR@aiworldwide.com>
2022-08-24 15:14:47 +0200

    Update workflow

--- server4/main.go
+++ server4/main.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("<h1>Full Cycle Esquenta</h1>"))
+		w.Write([]byte("<h1>Full Cycle Esquenta server 4</h1>"))
 	})
 	http.ListenAndServe(":8080", nil)
 }
